a23a41a1227fbfa52b65a2530b73b2d3cbba8290
dibya290 <dibyajyoti@gotisheel.com>
2025-07-28 16:02:12 +0530

    ad

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { ReactNode } from 'react';
 import SessionWrapper from '@/components/SessionWrapper';
 import { Toaster } from 'react-hot-toast';
 import { ThemeProvider } from '@/components/ThemeProvider';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
@@ -17,6 +18,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         >
           <SessionWrapper>
             {children}
+             <SpeedInsights />
             <Toaster position="top-right" />
           </SessionWrapper>
         </ThemeProvider>
